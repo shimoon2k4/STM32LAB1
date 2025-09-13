8b39f24c5f198b1d32cc46effdda93b184768dd0
Hoàng Sỹ Xuân Sơn <son.hoang0852@hcmut.edu.vn>
2025-09-13 17:47:12 +0700

    update

---
+++ LAB1/Core/Inc/ex1.h
@@ -0,0 +1,13 @@
+/*
+ * ex1.h
+ *
+ *  Created on: Sep 13, 2025
+ *      Author: Lenovo
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+
+
+#endif /* INC_EX1_H_ */
---
+++ LAB1/Core/Src/ex1.c
@@ -0,0 +1,8 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Sep 13, 2025
+ *      Author: Lenovo
+ */
+
+
